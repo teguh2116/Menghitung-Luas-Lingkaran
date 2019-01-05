88fa4d21e30c2ec7bbfbad4b3a154df761b27d9f
teguh2116 <43785306+teguh2116@users.noreply.github.com>
2019-01-06 01:27:54 +0700

    Add files via upload

---
+++ Menghitung Luas Lingkaran.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int r;
+    float phi=3.14, luas;
+    cout<< "===== Menghitung Luas Lingkaran =====\n\n";
+    cout<< " Input Jari-Jari :";
+    cin>>r;
+    luas=phi*r*r;
+    cout<< " Luas Lingkaran :"<<luas;
+
+    char LG;
+    cout<<"\n \n\n Apakah anda ingin mengulang program ini kembali [ Y/T ] ?";cin>>LG;
+if (LG=='Y' || LG=='y')main();
+else if (LG=='T' || LG=='t') goto x;
+x:
+    return 0;
+}
